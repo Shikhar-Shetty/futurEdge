ca95faab5ad0f09009b4e5ab3f89f06f510ba804
Shikhar Shetty <shettyshikhar99@gmail.com>
2025-05-04 10:56:44 +0530

    Fixed some middleware Issues

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,20 +6,24 @@ export async function middleware(request: NextRequest) {
   const token = await getToken({ req: request });
   const url = request.nextUrl;
 
-  if (url.pathname === "/dashboard") {
-      if (!token) {
-          return NextResponse.redirect(new URL("/sign-in", request.url));
-      }
-      return NextResponse.next();
+  if (url.pathname === "/sign-in" && token) {
+    return NextResponse.redirect(new URL("/dashboard", request.url));
   }
 
-  if (url.pathname.startsWith("/dashboard")) {
-      if (!token) {
-          return NextResponse.redirect(new URL("/sign-in", request.url));
-      }
-      else {
-        return NextResponse.redirect(new URL("/dashboard", request.url))
-      }
+  if (url.pathname === "/dashboard" || url.pathname.startsWith("/dashboard/")) {
+    if (!token) {
+      return NextResponse.redirect(new URL("/sign-in", request.url));
+    }
+    return NextResponse.next();
   }
+
   return NextResponse.next();
+}
+
+export const config = {
+  matcher: [
+    '/dashboard',
+    '/dashboard/:path*',
+    '/sign-in',
+  ],
 }
